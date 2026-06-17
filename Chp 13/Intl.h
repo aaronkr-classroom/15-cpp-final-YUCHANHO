e479f915b3446154fe3fe3112a4fe07735dd5e16
YUCHANHO <anteambulo02@gmail.com>
2026-06-17 15:44:42 +0900

    15 finaltest 20211957 유찬호

---
+++ Chp 13/Intl.h
@@ -0,0 +1,25 @@
+#pragma once
+// Intl.h
+
+#ifndef GUARD_Intl_h
+#define GUARD_Intl_h
+
+#include <iostream>
+
+#include "Core.h"
+
+using namespace std;
+
+class Intl : public Core {
+public:
+	Intl() : toeic(0) { };
+	Intl(istream& is) { read(is); };
+	double grade() const;
+	istream& read(istream&);
+
+private:
+	double toeic; // 토익 점수
+	Intl* clone() const { return new Intl(*this); }
+};
+
+#endif
